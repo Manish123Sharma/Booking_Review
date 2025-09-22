0c3fd9a6610378f713878271fb3b9fea815b93ec
Manish <manish2003sharma@gmail.com>
2025-09-22 23:46:44 +0530

    Initial Push

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI,)
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.error('MongoDB Connection Error:', err));
+
+module.exports = mongoose;
