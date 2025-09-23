a87965b76c99c6207d0c3cc69aebef481c03a68c
Manish <manish2003sharma@gmail.com>
2025-09-23 11:10:27 +0530

    booking_review

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const authRoutes = require('./src/routes/authRoutes');
 
 dotenv.config();
 require('./src/config/db');
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
